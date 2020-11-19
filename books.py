8c31ef3b6bafb1e50c74008ee4fe9152fb1d51ce
Harry <cashel.harry@gmail.com>
2020-11-19 21:38:59 +1100

    set url prefix

--- books.py
+++ books.py
@@ -1,9 +1,9 @@
 from database import cursor, connection
 from flask import Blueprint, request, jsonify, abort
-books = Blueprint("books", __name__)
+books = Blueprint("books", __name__, url_prefix="/books")
 
 
-@books.route("/books", methods=["GET"])
+@books.route("/", methods=["GET"])
 def book_index():
     # Return all books
     sql = "SELECT * FROM books"
@@ -12,7 +12,7 @@ def book_index():
     return jsonify(books)
 
 
-@books.route("/books", methods=["POST"])
+@books.route("/", methods=["POST"])
 def book_create():
     # Create a new book
     sql = "INSERT INTO books (title) VALUES (%s);"
@@ -25,7 +25,7 @@ def book_create():
     return jsonify(book)
 
 
-@books.route("/books/<int:id>", methods=["GET"])
+@books.route("/<int:id>", methods=["GET"])
 def book_show(id):
     # Return a single book
     sql = "SELECT * FROM books WHERE id = %s;"
@@ -34,7 +34,7 @@ def book_show(id):
     return jsonify(book)
 
 
-@books.route("/books/<int:id>", methods=["PUT", "PATCH"])
+@books.route("/<int:id>", methods=["PUT", "PATCH"])
 def book_update(id):
     # Update a book
     sql = "UPDATE books SET title = %s WHERE id = %s;"
@@ -47,7 +47,7 @@ def book_update(id):
     return jsonify(book)
 
 
-@books.route("/books/<int:id>", methods=["DELETE"])
+@books.route("/<int:id>", methods=["DELETE"])
 def book_delete(id):
     # Delete a book
     sql = "SELECT * FROM books WHERE id = %s;"
