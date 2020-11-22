e92149bd647fb64db6a24dea1844b5497b36d273
Harry <cashel.harry@gmail.com>
2020-11-22 19:14:32 +1100

    Updated routes/endpoints to use the ORM SQLAlchemy (rather than raw SQL)

--- controllers/books.py
+++ controllers/books.py
@@ -1,62 +1,53 @@
 from models.Book import Book
 from main import db
-from flask import Blueprint, request, jsonify, abort
-from schemas.BookSchema import books_schema
-books = Blueprint("books", __name__, url_prefix="/books")
+from schemas.BookSchema import book_schema, books_schema
+from flask import Blueprint, request, jsonify
+
+books = Blueprint('books', __name__, url_prefix="/books")
 
 
 @books.route("/", methods=["GET"])
 def book_index():
-    # Return all books
+    # Retrieve all books
     books = Book.query.all()
     return jsonify(books_schema.dump(books))
 
-# 
-# @books.route("/", methods=["POST"])
-# def book_create():
-#     # Create a new book
-#     sql = "INSERT INTO books (title) VALUES (%s);"
-#     cursor.execute(sql, (request.json["title"],))
-#     connection.commit()
-# 
-#     sql = "SELECT * FROM BOOKS ORDER BY ID DESC LIMIT 1"
-#     cursor.execute(sql)
-#     book = cursor.fetchone()
-#     return jsonify(book)
-# 
-# 
-# @books.route("/<int:id>", methods=["GET"])
-# def book_show(id):
-#     # Return a single book
-#     sql = "SELECT * FROM books WHERE id = %s;"
-#     cursor.execute(sql, (id,))
-#     book = cursor.fetchone()
-#     return jsonify(book)
-# 
-# 
-# @books.route("/<int:id>", methods=["PUT", "PATCH"])
-# def book_update(id):
-#     # Update a book
-#     sql = "UPDATE books SET title = %s WHERE id = %s;"
-#     cursor.execute(sql, (request.json["title"], id))
-#     connection.commit()
-# 
-#     sql = "SELECT * FROM books WHERE id = %s"
-#     cursor.execute(sql, (id,))
-#     book = cursor.fetchone()
-#     return jsonify(book)
-# 
-# 
-# @books.route("/<int:id>", methods=["DELETE"])
-# def book_delete(id):
-#     # Delete a book
-#     sql = "SELECT * FROM books WHERE id = %s;"
-#     cursor.execute(sql, (id,))
-#     book = cursor.fetchone()
-# 
-#     if book:
-#         sql = "DELETE FROM books WHERE id = %s;"
-#         cursor.execute(sql, (id,))
-#         connection.commit()
-# 
-#     return jsonify(book)
+
+@books.route("/", methods=["POST"])
+def book_create():
+    # Create a new book
+    book_fields = book_schema.load(request.json)
+
+    new_book = Book()
+    new_book.title = book_fields["title"]
+
+    db.session.add(new_book)
+    db.session.commit()
+
+    return jsonify(book_schema.dump(new_book))
+
+
+@books.route("/<int:id>", methods=["GET"])
+def book_show(id):
+    # Return a single book
+    book = Book.query.get(id)
+    return jsonify(book_schema.dump(book))
+
+
+@books.route("/<int:id>", methods=["PUT", "PATCH"])
+def book_update(id):
+    # Update a book
+    books = Book.query.filter_by(id=id)
+    book_fields = book_schema.load(request.json)
+    books.update(book_fields)
+    db.session.commit()
+
+    return jsonify(book_schema.dump(books[0]))
+
+
+@books.route("/<int:id>", methods=["DELETE"])
+def book_delete(id):
+    # Delete a book
+    book = Book.query.get(id)
+    db.session.delete(book)
+    db.session.commit()
