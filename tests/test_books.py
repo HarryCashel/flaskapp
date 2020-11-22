6476e2df2309575d07513f34938973d33ac2d392
Harry <cashel.harry@gmail.com>
2020-11-22 23:22:58 +1100

    filled out TestBooks class

--- tests/test_books.py
+++ tests/test_books.py
@@ -1,5 +1,26 @@
 import unittest
+from main import create_app, db
+
 
 class TestBooks(unittest.TestCase):
+
+    @classmethod
+    def setUp(cls):
+        cls.app = create_app()
+        cls.app_context = cls.app.app_context()
+        cls.client = cls.app.test_client()
+        db.create_all()
+
+    @classmethod
+    def tearDown(cls):
+        db.session.remove()
+        db.drop_all()
+        cls.app_context.pop()
+
     def test_book_index(self):
-        pass
+        response = self.client.get("/books/")
+
+        data = response.get_json()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(data, list)
