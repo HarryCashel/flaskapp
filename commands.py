00a20dfea1347cd44fc769a202358f326c845511
Harry <cashel.harry@gmail.com>
2020-11-22 22:31:27 +1100

    created flask CLI seed function + updated requirements.txt

--- commands.py
+++ commands.py
@@ -15,3 +15,17 @@ def create_db():
 def drop_db():
     db.drop_all()
     print("Tables deleted!")
+
+@db_commands.cli.command("seed")
+def seed_db():
+    from models.Book import Book
+    from faker import Faker
+    faker = Faker()
+
+    for i in range(20):
+        book = Book()
+        book.title = faker.catch_phrase()
+        db.session.add(book)
+
+    db.session.commit()
+    print("Tables seeded!")
